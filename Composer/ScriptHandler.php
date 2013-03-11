a342cbc7e96fa575e9bbc5bb53094a83378dde8d
Ruben Nijveld <ruben@gewooniets.nl>
2013-03-11 14:54:14 +0100

    Updated files

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tg\OkoaBundle\Composer;
+
+use Symfony\Component\ClassLoader\ClassCollectionLoader;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Composer\Script\CommandEvent;
+
+class ScriptHandler
+{
+    public static function install(CommandEvent $event)
+    {
+
+    }
+
+    public static function update(CommandEvent $event)
+    {
+
+    }
+}
